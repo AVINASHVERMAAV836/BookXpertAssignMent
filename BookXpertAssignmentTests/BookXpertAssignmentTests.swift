056123f767a1efdd676cec08469c84acbc71e7e2
Avinash-at-880755199573 <avinash@bizionictech.com>
2026-07-30 22:46:58 +0530

    add unit test case module

---
+++ BookXpertAssignmentTests/BookXpertAssignmentTests.swift
@@ -0,0 +1,18 @@
+//
+//  BookXpertAssignmentTests.swift
+//  BookXpertAssignmentTests
+//
+//  Created by Avinash Verma on 30/07/26.
+//
+
+import Testing
+
+struct BookXpertAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
---
+++ BookXpertAssignmentTests/BookXpertAssignmentTestsClass.swift
@@ -0,0 +1,17 @@
+//
+//  BookXpertAssignmentTestsClass.swift
+//  BookXpertAssignmentTests
+//
+//  Created by Avinash Verma on 30/07/26.
+//
+
+import XCTest
+
+@testable import BookXpertAssignment
+
+final class BookXpertAssignmentTestsClass: XCTestCase {
+
+    func testAddition() {
+        XCTAssertEqual(2 + 2, 4)
+    }
+}
